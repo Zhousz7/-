0789b1494bac7d1566f24a3677880af3be99c74d
Zhousz7 <2630015080@qq.com>
2023-04-03 21:59:42 +0800

    Matplotlib and Plotly learning

---
+++ 4_rw_visual.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+# 只要程序处于活跃状态，就不断地模拟随机漫步
+while True:
+    # 创建一个RandomWalk实例
+    rw = RandomWalk(50_000)
+    rw.fill_walk()
+
+    # 将所有点都描绘出来
+    plt.style.use('classic')
+    fig, ax = plt.subplots(figsize=(15, 9), dpi=128)
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+               edgecolor='none', s=1)
+
+    # 突出起点和终点
+    ax.scatter(0, 0, c='green', edgecolor='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolor='none',
+               s=100)
+
+    # 隐藏坐标轴
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
